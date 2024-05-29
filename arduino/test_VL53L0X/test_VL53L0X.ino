327bea010cf488df202c80393975712433bedb6d
rock1 <dlmarcy@syr.edu>
2024-05-29 16:55:30 -0400

    urdf topics match arduino topics

--- arduino/test_VL53L0X/test_VL53L0X.ino
+++ arduino/test_VL53L0X/test_VL53L0X.ino
@@ -12,12 +12,12 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  sensor.setTimeout(15);
+  sensor.setTimeout(500);
   if (!sensor.init()) {
     Serial.println("Failed to detect and initialize sensor!");
     while (1) {}
   }
-  sensor.setMeasurementTimingBudget(200000);
+  //sensor.setMeasurementTimingBudget(500000);
 
   // Start continuous back-to-back mode (take readings as
   // fast as possible).  To use continuous timed mode
@@ -28,8 +28,8 @@ void setup() {
 
 void loop() {
   // When used with ROS the sensor will be sampled at 5Hz
-  if ((millis()-check_time) > 200) {
-    check_time += 200;
+  if ((millis()-check_time) > 500) {
+    check_time += 500;
     Serial.print(sensor.readRangeContinuousMillimeters());
     if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
     Serial.println();
