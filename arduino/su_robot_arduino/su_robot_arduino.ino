3ada45c49c0b77e96a47c9da687eb7fb1a611423
dlmarcy <96996188+dlmarcy@users.noreply.github.com>
2024-07-12 16:20:41 -0400

    Update su_robot_arduino.ino

--- arduino/su_robot_arduino/su_robot_arduino.ino
+++ arduino/su_robot_arduino/su_robot_arduino.ino
@@ -1,3 +1,8 @@
+/*
+  cse400_arduino_F24 SU 7/12/2024
+  Syracuse University Robotics Programming Course
+*/
+
 #include "MicroROSArduino.h"
 #include <VL53L0X.h>
 #include <Adafruit_INA219.h>
@@ -8,18 +13,32 @@
 #include <Encoder.h>
 #include <AccelStepper.h>
 
-// define pin numbers
+// GPIO pin definitions
 int Mot_L_EN = 4;
 int Mot_L_PH = 5;
 int Mot_R_EN = 3;
 int Mot_R_PH = 2;
-int Encode_L1 = 12;
-int Encode_L2 = 6;
+int Encode_L1 = 17;
+int Encode_L2 = 16;
 int Encode_R1 = 0;
 int Encode_R2 = 1;
+int Step1_Or = 20;
+int Step1_Ye = 6;
+int Step1_Pi = 12;
+int Step1_Bl = 11;
+int Step2_Or = 7;
+int Step2_Ye = 8;
+int Step2_Pi = 9;
+int Step2_Bl = 10;
+int LED_GRN1 = 14; // SOC LEDs
+int LED_RED1 = 15;
+int LED_GRN2 = 22; // Current LEDs
+int LED_RED2 = 23;
+int Teensy_LED = 13;
+int Push_But = 21;
 
-int left = 1;
-int right = 2;
+const int LEFT = 1;
+const int RIGHT = 2;
 
 // create a micro ros object
 MicroROSArduino micro_ros;
@@ -32,32 +51,31 @@ long previous_r = 0;
 Encoder encoder_left(Encode_L1, Encode_L2); 
 Encoder encoder_right(Encode_R2, Encode_R1); 
 
+// define controller variables and objects
 // default sample time for the PID controller is 100ms
 // can be changed with object.SetSampleTime(int NewSampleTime);
 // change setpoint with object.Setpoint(double v);
+double input_l = 0.0;
+double output_l = 0.0;
+double input_r = 0.0;
+double output_r = 0.0;
 double Kp = 5.0, Ki = 100.0, Kd = 0.0;
 PID_v2 control_left(Kp, Ki, Kd, PID::Direct);
 PID_v2 control_right(Kp, Ki, Kd, PID::Direct);
+AccelStepper control_shoulder(AccelStepper::FULL4WIRE, Step2_Or, Step2_Pi, Step2_Ye, Step2_Bl);
+AccelStepper control_elbow(AccelStepper::FULL4WIRE, Step1_Or, Step1_Pi, Step1_Ye, Step1_Bl);
 
-// controller variables
-double input_l = 0.0;
-double output_l = 0.0;
-double setpoint_l = 0.0;
-double input_r = 0.0;
-double output_r = 0.0;
-double setpoint_r = 0.0;
-
+// define scaling variables
 // The documentation says the motor encoders count 48 edges per revolution
 // The gear ratio is 99:1, 99*48 = 4752
 // The wheels are 80 mm diameter
-float scale_counts_pos = 99*48/(2*3.14159); // encoder counts per radian
-float scale_radians_pos = 1.0/scale_counts_pos;  // radians per encoder count
+const float scale_counts_pos = 99*48/(2*3.14159); // encoder counts per radian
+const float scale_radians_pos = 1.0/scale_counts_pos;  // radians per encoder count
 // The loop for measuring velocity counts for 1/10 second
-float scale_counts_vel = scale_counts_pos*0.1;
-float scale_radians_vel = 1.0/scale_counts_vel;
-
-double shoulder_setpoint = 0;
-double elbow_setpoint = 0;
+const float scale_counts_vel = scale_counts_pos*0.1;
+const float scale_radians_vel = 1.0/scale_counts_vel;
+const float scale_steps_arm = 1019.0/3.14159; // steps per radian
+const float scale_radians_arm = 1.0/scale_steps_arm; // radians per step
 
 void battery_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
   micro_ros.battery_msg.voltage = 10;
@@ -100,7 +118,7 @@ void commander_cb(const void * msgin)
 }
 
 void set_PWM(int motor, int value) {
-  if (motor == left) {
+  if (motor == LEFT) {
     if (value < 0) {
       digitalWrite(Mot_L_PH, HIGH);  // left backward
       analogWrite(Mot_L_EN, -value);
@@ -149,7 +167,7 @@ void loop() {
 
   // keep wheels spinning
   output_l = control_left.Run(input_l);
-  set_PWM(left, output_l);
+  set_PWM(LEFT, output_l);
   output_r = control_right.Run(input_r);
-  set_PWM(right, output_r);
+  set_PWM(RIGHT, output_r);
 }
