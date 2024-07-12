6479a2317d91fa27f751f24bba3ea832c6bd6168
dlmarcy <96996188+dlmarcy@users.noreply.github.com>
2024-07-12 17:27:59 -0400

    Update su_robot_arduino.ino

--- arduino/su_robot_arduino/su_robot_arduino.ino
+++ arduino/su_robot_arduino/su_robot_arduino.ino
@@ -43,8 +43,29 @@ const int RIGHT = 2;
 // create a micro ros object
 MicroROSArduino micro_ros;
 
+// define battery variables and objects
+uint8_t battery_id;
+Adafruit_INA219 power_monitor;
+
+// define range variables and objects
+uint8_t range_id;
+VL53L0X range_sensor;
+
+// define imu variables and objects
+uint8_t imu_id;
+int eeAddress = 0;
+long bnoID;
+adafruit_bno055_offsets_t calibrationData;
+sensor_t sensor;
+uint8_t system_cal, gyro_cal, accel_cal, mag_cal = 0;
+imu::Quaternion quat;
+imu::Vector<3> gyro;
+imu::Vector<3> accel;
+imu::Vector<3> magnet;
+Adafruit_BNO055 imu_sensor = Adafruit_BNO055(55);  
+
 // define the encoder variables and objects
-uint8_t joint_state_id;
+uint8_t joint_states_id;
 long encoder_l = 0;
 long encoder_r = 0;
 long previous_l = 0;
@@ -78,27 +99,6 @@ const float scale_radians_vel = 1.0/scale_counts_vel;
 const float scale_steps_arm = 1019.0/3.14159; // steps per radian
 const float scale_radians_arm = 1.0/scale_steps_arm; // radians per step
 
-// define battery variables and objects
-uint8_t battery_id;
-Adafruit_INA219 power_monitor;
-
-// define range variables and objects
-uint8_t range_id;
-VL53L0X range_sensor;
-
-// define imu variables and objects
-uint8_t imu_id;
-int eeAddress = 0;
-long bnoID;
-adafruit_bno055_offsets_t calibrationData;
-sensor_t sensor;
-uint8_t system_cal, gyro_cal, accel_cal, mag_cal = 0;
-imu::Quaternion quat;
-imu::Vector<3> gyro;
-imu::Vector<3> accel;
-imu::Vector<3> magnet;
-Adafruit_BNO055 imu_sensor = Adafruit_BNO055(55);  
-
 void battery_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
   micro_ros.battery_msg[battery_id].voltage = power_monitor.getBusVoltage_V() + (power_monitor.getShuntVoltage_mV()/1000.0);
   micro_ros.battery_msg[battery_id].current = power_monitor.getCurrent_mA()/-1000.0;
@@ -106,15 +106,28 @@ void battery_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
 }
 
 void range_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
-  micro_ros.range_msg.range = range_sensor.readRangeContinuousMillimeters()*0.001;
-  if (micro_ros.range_msg.range > 1.0) { range_msg.range = 1.0; }
+  float range = range_sensor.readRangeContinuousMillimeters()*0.001;
+  micro_ros.range_msg[range_id].range = range;
+  if (range > 1.0) { micro_ros.range_msg[range_id].range = 1.0; } else { micro_ros.range_msg[range_id].range = range; }
   micro_ros.publishRange(range_id);
 }
 
 void imu_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
-  micro_ros.imu_msg.orientation.w = 0.707;
-  micro_ros.imu_msg.orientation.z = 0.707;
-  micro_ros.publishImu();
+  imu_sensor.getCalibration(&system_cal, &gyro_cal, &accel_cal, &mag_cal);
+  quat = imu_sensor.getQuat();
+  gyro = imu_sensor.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  accel = imu_sensor.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  micro_ros.imu_msg[imu_id].orientation.w = quat.w();
+  micro_ros.imu_msg[imu_id].orientation.x = quat.x();
+  micro_ros.imu_msg[imu_id].orientation.y = quat.y();
+  micro_ros.imu_msg[imu_id].orientation.z = quat.z();
+  micro_ros.imu_msg[imu_id].angular_velocity.x = gyro.x();
+  micro_ros.imu_msg[imu_id].angular_velocity.y = gyro.y();
+  micro_ros.imu_msg[imu_id].angular_velocity.z = gyro.z();
+  micro_ros.imu_msg[imu_id].linear_acceleration.x = accel.x();
+  micro_ros.imu_msg[imu_id].linear_acceleration.y = accel.y();
+  micro_ros.imu_msg[imu_id].linear_acceleration.z = accel.z();
+  micro_ros.publishImu(imu_id);
 }
 
 void joint_state_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
@@ -128,7 +141,7 @@ void joint_state_timer_cb(rcl_timer_t * timer, int64_t last_call_time) {
   micro_ros.joint_state_msg.velocity.data[0] = input_l;
   micro_ros.joint_state_msg.position.data[1] = scale_radians_pos*encoder_r;
   micro_ros.joint_state_msg.velocity.data[1] = input_r;
-  micro_ros.publishJointState(joint_state_id);
+  micro_ros.publishJointState(joint_states_id);
 }
 
 void commander_cb(const void * msgin)
@@ -162,27 +175,94 @@ void set_PWM(int motor, int value) {
 }
 
 void setup() {
-  //micro_ros.beginBatteryBroadcaster(&battery_timer_cb);
-  //micro_ros.beginRangeBroadcaster(&range_timer_cb);
-  micro_ros.beginImuBroadcaster(&imu_timer_cb);
-  micro_ros.beginJointStateBroadcaster(&joint_state_timer_cb);
-  micro_ros.beginJointStateCommander(&commander_cb);
+  Wire.begin();
+  
+  // set up LEDs and pushbutton
+  analogWrite(LED_RED1, 0); // red SOC LED
+  analogWrite(LED_GRN1, 0); // green SOC LED
+  analogWrite(LED_RED2, 0); // red current LED
+  analogWrite(LED_GRN2, 0); // green current LED
+  pinMode(Teensy_LED, OUTPUT); // Teensy LED
+  digitalWrite(Teensy_LED, LOW); 
+  pinMode(Push_But, INPUT_PULLDOWN); // Pushbutton
 
-  // setup wheels
+  // set up power monitor
+  battery_id = micro_ros.beginBroadcaster(MicroROSArduino::BATTERY, "battery", 1.0, &battery_timer_cb);
+  rosidl_runtime_c__String__assignn(&micro_ros.battery_msg[battery_id].header.frame_id, "battery", 7);
+  micro_ros.battery_msg[battery_id].present = true;
+  micro_ros.battery_msg[battery_id].power_supply_technology = sensor_msgs__msg__BatteryState__POWER_SUPPLY_TECHNOLOGY_LIFE;
+  micro_ros.battery_msg[battery_id].power_supply_health = sensor_msgs__msg__BatteryState__POWER_SUPPLY_HEALTH_GOOD;
+  micro_ros.battery_msg[battery_id].design_capacity = 6.0;  //Ah
+  power_monitor.begin();
+
+  // set up range sensor
+  range_id = micro_ros.beginBroadcaster(MicroROSArduino::RANGE, "range", 5.0, &range_timer_cb);
+  rosidl_runtime_c__String__assignn(&micro_ros.range_msg[range_id].header.frame_id, "range_sensor", 12);
+  micro_ros.range_msg[range_id].radiation_type = sensor_msgs__msg__Range__INFRARED;
+  micro_ros.range_msg[range_id].field_of_view = 0.436;  // radians
+  micro_ros.range_msg[range_id].min_range = 0.01;
+  micro_ros.range_msg[range_id].max_range = 1.0;
+  range_sensor.setTimeout(15);
+  range_sensor.init();
+  range_sensor.setMeasurementTimingBudget(200000);
+  range_sensor.startContinuous();
+
+  // set up IMU
+  imu_id = micro_ros.beginBroadcaster(MicroROSArduino::IMU, "imu", 20.0, &imu_timer_cb);
+  rosidl_runtime_c__String__assignn(&micro_ros.imu_msg[0].header.frame_id, "imu", 3);
+  micro_ros.imu_msg[imu_id].orientation_covariance[0] = 1.0;
+  micro_ros.imu_msg[imu_id].orientation_covariance[4] = 1.0;
+  micro_ros.imu_msg[imu_id].orientation_covariance[8] = 1.0;
+  micro_ros.imu_msg[imu_id].angular_velocity_covariance[0] = 1.0;
+  micro_ros.imu_msg[imu_id].angular_velocity_covariance[4] = 1.0;
+  micro_ros.imu_msg[imu_id].angular_velocity_covariance[8] = 1.0;
+  micro_ros.imu_msg[imu_id].linear_acceleration_covariance[0] = 1.0;
+  micro_ros.imu_msg[imu_id].linear_acceleration_covariance[4] = 1.0;
+  micro_ros.imu_msg[imu_id].linear_acceleration_covariance[8] = 1.0;
+  imu_sensor.begin(); 
+  EEPROM.get(eeAddress, bnoID); // Check for calibration data in Teensy's EEPROM
+  imu_sensor.getSensor(&sensor);
+  if (bnoID == sensor.sensor_id) {
+    eeAddress += sizeof(long);
+    EEPROM.get(eeAddress, calibrationData);
+    imu_sensor.setSensorOffsets(calibrationData);
+	// Flash Red Green to indicate calib data found
+    analogWrite(LED_RED1, 255);
+    analogWrite(LED_GRN2, 255);
+  }
+  delay(100);  // Example sketch used a delay here
+  imu_sensor.setExtCrystalUse(true);
+
+  // set up encoders and broadcaster
+  String JointNames[4];
+  JointNames[0] = "gear_left_shaft";
+  JointNames[1] = "gear_right_shaft";
+  joint_states_id = micro_ros.beginBroadcaster(MicroROSArduino::eeJointState, "joint_states", 10.0, &joint_state_timer_cb, 4, JointNames);
+  micro_ros.beginJointStateCommander(&commander_cb, "commands", 2, JointNames);
+  encoder_left.write(0);
+  encoder_right.write(0);
+  
+  // set up PID controller, motors, and commander
   analogWrite(Mot_L_EN, 0); // left speed pin
   pinMode(Mot_L_PH, OUTPUT);  // left direction pin
   digitalWrite(Mot_L_PH, HIGH);  // left forward
   analogWrite(Mot_R_EN, 0); // right speed pin
   pinMode(Mot_R_PH, OUTPUT);  //right direction pin
   digitalWrite(Mot_R_PH, LOW);  // right forward
-  encoder_left.write(0);
-  encoder_right.write(0);
   // Clamps the output to a specific range. 0-255 by default
   control_left.SetOutputLimits(-255, 255);
   control_right.SetOutputLimits(-255, 255);
   // object.Start(input, current output, setpoint);
   control_left.Start(0, 0, 0);
   control_right.Start(0, 0, 0);
+ 
+  // set up arm stepper motors
+  control_shoulder.disableOutputs();
+  control_shoulder.setMaxSpeed(400);
+  control_shoulder.setAcceleration(10);
+  control_elbow.disableOutputs();
+  control_elbow.setMaxSpeed(400);
+  control_elbow.setAcceleration(10);
 }
 
 void loop() {
